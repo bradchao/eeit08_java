5ea816a0f7fece65a569fe6306b3cd210731fea0
mickeyyo55b <mickeyyo55b@gmail.com>
2025-06-23 14:34:40 +0800

    06231434

---
+++ src/tw/brad/tutor/Brad05.java
@@ -0,0 +1,16 @@
+package tw.brad.tutor;
+
+public class Brad05 {
+
+	public static void main(String[] args) {
+		// char 2^16, 0 ~ 65535
+		char c1 = 65;
+		char c2 = 66;
+		System.out.println(c2);
+		char c3 = '資';
+		
+		boolean a = true;
+		boolean b = false;
+	}
+
+}
