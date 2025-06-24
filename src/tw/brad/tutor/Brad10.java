ec5a8418cbc015e0c60bef5c106f0032e8f4b1a6
mickeyyo55b <mickeyyo55b@gmail.com>
2025-06-24 09:57:12 +0800

    06240957

---
+++ src/tw/brad/tutor/Brad10.java
@@ -0,0 +1,28 @@
+package tw.brad.tutor;
+
+public class Brad10 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int[] ary;
+		ary = new int[3];
+		System.out.println(ary[2]);
+		System.out.println(ary.length);
+		ary[0] = 12;
+		ary[2] = 33;
+		for (int i = 0; i<ary.length; i++) {
+			System.out.println(ary[i]);
+		}
+		System.out.println("----");
+		for (int v : ary) {
+			System.out.println(v);
+		}
+		System.out.println("----");
+		System.out.println(a);
+		System.out.println(ary);
+		
+		
+		
+	}
+
+}
