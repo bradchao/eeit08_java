e58372f2c90313890348bb2cbda41ee89d063f50
mickeyyo55b <mickeyyo55b@gmail.com>
2025-06-24 14:23:51 +0800

    06241423

---
+++ src/tw/brad/tutor/Brad16.java
@@ -0,0 +1,17 @@
+package tw.brad.tutor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Brad16 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> poker = new ArrayList<>();
+		for (int i=0; i<10; i++) poker.add(i);
+		Collections.shuffle(poker);
+		for (Integer card: poker) {
+			System.out.println(card);
+		}
+	}
+
+}
