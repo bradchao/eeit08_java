025864da6547ac12485218b6cce1fdb6a3a63869
mickeyyo55b <mickeyyo55b@gmail.com>
2025-06-26 11:55:55 +0800

    06261200

--- src/tw/brad/tutor/GuessGame.java
+++ src/tw/brad/tutor/GuessGame.java
@@ -1,6 +1,8 @@
 package tw.brad.tutor;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -8,10 +10,11 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class GuessGame extends JFrame{
+public class GuessGame extends JFrame implements ActionListener{
 	private JButton guess;
 	private JTextField input;
 	private JTextArea log;
+	private String answer;
 	
 	public GuessGame() {
 		super("Guess Number Game");
@@ -29,15 +32,48 @@ public class GuessGame extends JFrame{
 		
 		add(top, BorderLayout.NORTH);
 		
+		guess.addActionListener(this);
 		
 		
 		setSize(640, 480);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		initGame();
 	}
 
 	public static void main(String[] args) {
 		new GuessGame();
+	}
+
+	private void initGame() {
+		answer = createAnswer(3);
+		System.out.println(answer);
+	}
+	
+	private String createAnswer(int d) {
+		final int nums = 10;
+		int[] poker = new int[nums];
+		for (int i=0; i<poker.length; i++) poker[i] = i;
+		
+		for (int i = nums -1; i > 0; i--) {
+			int r = (int)(Math.random()*(i+1));
+			// poker[r] <=> poker[i]
+			int temp = poker[r];
+			poker[r] = poker[i];
+			poker[i] = temp;
+		}
+		
+		StringBuffer sb = new StringBuffer();
+		for (int i=0; i<d; i++) sb.append(poker[i]);
+		
+		return sb.toString();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("OK");
+		
 	}
 
 }
