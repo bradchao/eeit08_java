50f8166a9e79a106d423e85e0f38cd109d1faa77
mickeyyo55b <mickeyyo55b@gmail.com>
2025-06-30 12:04:13 +0800

    06301204

---
+++ src/tw/brad/apis/Line.java
@@ -0,0 +1,19 @@
+package tw.brad.apis;
+
+import java.util.ArrayList;
+
+public class Line {
+	private ArrayList<Point> points;
+	public Line() {
+		points = new ArrayList<>();
+	}
+	public ArrayList<Point> getPoints() {
+		return points;
+	}
+	public void addPoint(Point p) {
+		points.add(p);
+	}
+	public Point getPoint(int index) {
+		return points.get(index);
+	}
+}
--- src/tw/brad/apis/MyDrawer.java
+++ src/tw/brad/apis/MyDrawer.java
@@ -5,12 +5,21 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 import javax.swing.DebugGraphics;
 import javax.swing.JPanel;
 
 public class MyDrawer extends JPanel{
+	private ArrayList<Point> line;
+	
+	//private ArrayList<ArrayList<Point>> lines;
+	private ArrayList<Line> lines;
+	
 	public MyDrawer() {
+		line = new ArrayList<>();
+		
 		setBackground(Color.YELLOW);
 		
 		MyMouseAdapter adapter = new MyMouseAdapter();
@@ -27,13 +36,29 @@ public class MyDrawer extends JPanel{
 		g2d.setStroke(new BasicStroke(4));
 		
 		g2d.setColor(Color.BLUE);
-		g2d.drawLine(0, 0, 100, 200);
+		
+		for (int i=1; i<line.size(); i++) {
+			Point p1 = line.get(i-1);
+			Point p2 = line.get(i);
+			g2d.drawLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+		}
 		
 		
 	}
 	
 	private class MyMouseAdapter extends MouseAdapter {
+		@Override
+		public void mousePressed(MouseEvent e) {
+			Point p = new Point(e.getX(), e.getY());
+			line.add(p);
+		}
 		
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			Point p = new Point(e.getX(), e.getY());
+			line.add(p);
+			repaint();
+		}
 	}
 	
 	
