7d61797df650f1b711e294fdb3e56ebcfea80350
mickeyyo55b <mickeyyo55b@gmail.com>
2025-07-01 12:01:56 +0800

    07011201

--- src/tw/brad/tutor/Brad41.java
+++ src/tw/brad/tutor/Brad41.java
@@ -1,16 +1,24 @@
 package tw.brad.tutor;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Brad41 {
 
 	public static void main(String[] args) {
 		File f1 = new File("d:/brad/dir1/file1");
-		if (!f1.exists()) {
-//			if (f1.createNewFile()) {
-//				
-//			}
-		}
+//		if (!f1.exists()) {
+			try {
+				if (f1.createNewFile()) {
+					System.out.println("OK");
+				}else {
+					System.out.println("XX");
+				}
+				System.out.println("OK2");
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+//		}
 			
 	}
 
--- src/tw/brad/tutor/Brad44.java
+++ src/tw/brad/tutor/Brad44.java
@@ -2,6 +2,23 @@ package tw.brad.tutor;
 
 public class Brad44 {
 	public static void main(String[] args) {
-
+		Brad441 obj = new Brad441();
+		try {
+			obj.m1();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
+class Brad441 {
+	void m1() throws Exception {
+		m2();
+	}
+	void m2() throws Exception{
+		m3();
+	}
+	void m3() throws Exception{
+		System.out.println("m3()");
+		throw new Exception();
 	}
 }
