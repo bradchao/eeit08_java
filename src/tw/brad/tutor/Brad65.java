30b884a274ec41097a000e19385c72b1ce5a7e7b
mickeyyo55b <mickeyyo55b@gmail.com>
2025-07-02 15:28:20 +0800

    07021528

--- src/tw/brad/tutor/Brad65.java
+++ src/tw/brad/tutor/Brad65.java
@@ -1,10 +1,41 @@
 package tw.brad.tutor;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Brad65 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		while(true) {
+			try {
+				ServerSocket server = new ServerSocket(7777);
+				
+					Socket socket =  server.accept();
+					
+					BufferedInputStream bin = new BufferedInputStream(socket.getInputStream());
+					byte[] allData =  bin.readAllBytes();
+					bin.close();
+					
+					String urip = socket.getInetAddress().getHostAddress();
+					BufferedOutputStream bout = new BufferedOutputStream(
+						new FileOutputStream(String.format("upload/%s.jpg", urip)));
+					bout.write(allData);
+					bout.flush();
+					bout.close();
+					
+					socket.close();
+				
+				server.close();
+				System.out.println("Receive OK");
+			} catch (Exception e) {
+				e.printStackTrace();
+				break;
+			}
+		}
+		
 	}
 
 }
