02b5d1701145252db42ff58f3bc8ed8166586c2e
mickeyyo55b <mickeyyo55b@gmail.com>
2025-07-03 12:00:52 +0800

    07031200

--- src/tw/brad/tutor/Brad69.java
+++ src/tw/brad/tutor/Brad69.java
@@ -2,21 +2,30 @@ package tw.brad.tutor;
 
 public class Brad69 {
 	public static void main(String[] args) {
-		MyThread mt1 = new MyThread();
-		MyThread mt2 = new MyThread();
+		MyThread mt1 = new MyThread("A");
 		mt1.start();
-		mt2.start();		
 		System.out.println("main");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		}
+		System.out.println("-----");
+		mt1.interrupt();
 	}
 }
 class MyThread extends Thread {
+	private String name;
+	
+	MyThread(String name){this.name = name;}
 	@Override
 	public void run() {
 		for (int i=0; i<20; i++) {
-			System.out.println(i);
+			System.out.printf("%s: %d\n", name, i);
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
+				System.out.println("interrupt");
+				break;
 			}
 		}
 	}
